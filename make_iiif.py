479ec00b49c988f8273b680add20d81a6dda1ed2
csae8092 <peter.andorfer@oeaw.ac.at>
2021-08-30 07:35:01 +0200

    description, logo, license ...

--- make_iiif.py
+++ make_iiif.py
@@ -1,47 +1,43 @@
 import os
-import glob
 import json
 
-from PIL import Image
+from utils import gsheet_to_df
+
 
 IIIF_SERVER_URL = "https://iiif.acdh.oeaw.ac.at/amp/"
 PROJECT_URI = "https://auden-musulin"
 PROJECT_MD = {
     "@context": "http://iiif.io/api/presentation/3/context.json",
-    "id": f"{PROJECT_URI}/manifest.json",
+    "id": "https://auden-musulin-papers.github.io/amp-process/manifest.json",
     "type": "Manifest",
     "label": {
         "en": [
             "Scans from Auden Musulin Project"
         ]
     },
+    "description": "Stella Musulin (1915-1996), née Stella Lloyd-Philipps, was a Welsh-Austrian writer, translator, journalist and broadcaster. Resident at the Musulin family estate at Schloss Fridau, Ober-Grafendorf, and Vienna, she was one of the closest friends of the Anglo-American poet W. H. Auden (1907-1973), with whom she corresponded frequently from the early 1960s, when Auden had settled in Kirchstetten, Lower Austria, until his death. The letters and literary papers by Auden preserved in Musulin’s estate provide revealing insights into one of Auden’s most prolific creative periods and Austria’s complex political, social and cultural history in the 1960s and 70s.",
+    "license": "https://creativecommons.org/licenses/by/4.0/",
+    "attribution": "Austrian Centre for Digitial Humanities and Cultural Heritage",
+    "logo": "https://shared.acdh.oeaw.ac.at/acdh-common-assets/images/acdh-ch-logo-with-text-grayscale.png",
+
     "behavior": [
         "non-paged"
     ],
     "items": []
 }
-IMG_DIR = os.environ.get('IMG', '')
-glob_pattern = f"{IMG_DIR}/*.jpg"
-print(glob_pattern)
-images = sorted(glob.glob(glob_pattern))
+SHEET_ID = os.environ.get('SHEETID')
+df = gsheet_to_df(SHEET_ID)
 
 
-def yield_img_dict(images):
-    for x in images:
-        item = {}
-        item['id'] = os.path.basename(x).split('.')[0]
-        with Image.open(x) as image:
-            item['width'], item['height'] = image.width, image.height
-        yield item
-
-
-for x in yield_img_dict(images):
+for i, x in df.iterrows():
+    image_id = f"amp_{x['id']:04}"
+    label = f"{x['has_title']} ({x['document_type']}); ID: {image_id}"
     item = {
-        "id": f"{PROJECT_URI}/canvas/{x['id']}",
+        "id": f"{PROJECT_URI}/canvas/{image_id}",
         "type": "Canvas",
         "label": {
             "en": [
-                f"ID: {x['id']}"
+                f"{label}"
             ]
         },
         "height": x['height'],
@@ -52,24 +48,24 @@ for x in yield_img_dict(images):
                 "type": "AnnotationPage",
                 "items": [
                     {
-                        "id": f"{PROJECT_URI}/{x['id']}",
+                        "id": f"{PROJECT_URI}/{image_id}",
                         "type": "Annotation",
                         "motivation": "painting",
                         "body": {
-                            "id": f"{IIIF_SERVER_URL}{x['id']}/full/max/0/default.jpg",
+                            "id": f"{IIIF_SERVER_URL}{image_id}/full/max/0/default.jpg",
                             "type": "Image",
                             "format": "image/jpeg",
                             "height": x['height'],
                             "width": x['width'],
                             "service": [
                                 {
-                                    "id": f"{IIIF_SERVER_URL}{x['id']}",
+                                    "id": f"{IIIF_SERVER_URL}{image_id}",
                                     "type": "ImageService3",
                                     "profile": "level1"
                                 }
                             ]
                         },
-                        "target": f"{PROJECT_URI}/canvas/{x['id']}"
+                        "target": f"{PROJECT_URI}/canvas/{image_id}"
                     }
                 ]
             }
