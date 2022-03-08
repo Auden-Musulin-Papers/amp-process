6510d0b3d2a3ca4c7a34b70eba961cf97f1ed80c
csae8092 <peter.andorfer@oeaw.ac.at>
2022-03-08 20:25:43 +0100

    added new images

--- copy_process.sh
+++ copy_process.sh
@@ -1,7 +1,7 @@
 # bin/bash
 
 echo "copy images from $SOURCE/*.tif into $IMG_TIF"
-cp $SOURCE/*.tif -n $IMG_TIF
+cp $SOURCE/*.tif -n -v $IMG_TIF
 echo "copy done"
 echo $(ls $IMG_TIF | wc -l) "images in $IMG_TIF"
 echo "start creating jpgs"
