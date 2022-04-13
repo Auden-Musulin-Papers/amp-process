769513b270341cfe564fc6f167ad04fe9ad01d05
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-13 09:30:55 +0200

    copy jpg back to acdh_resource

--- copy_process.sh
+++ copy_process.sh
@@ -6,3 +6,4 @@ echo "copy done"
 echo $(ls $IMG_TIF | wc -l) "images in $IMG_TIF"
 echo "start creating jpgs"
 for f in $IMG_TIF/*.tif; do  echo "Converting $f to $IMG/$(basename "$f" .tif).jpg"; convert  -quality 50% "$f"  "$IMG/$(basename "$f" .tif).jpg"; done
+cp $IMG/*.jpg -n -v $SOURCE_JPG
